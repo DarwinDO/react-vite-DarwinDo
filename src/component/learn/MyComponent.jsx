5ba4d0ee15f39623d9bfa14dd1c31df3b77393b0
DarwinDO <nguyendo836660@gmail.com>
2025-05-30 15:55:58 +0700

    video 30

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,8 +1,16 @@
 //() => {}
 // component = html + css + js
+
+import './style.css'
 const MyComponent = () => {
     return (
-        <div>DarwinDo has tested First Component</div>
+
+        <>
+            <div>DarwinDo has tested First Component</div>
+            <div className="child"> Test JSX and Fragment</div>
+        </>
+
+
     )
 }
 
