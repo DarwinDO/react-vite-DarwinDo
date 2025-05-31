f7f63aef8b5858f1da5275f228567ba553f68ac0
DarwinDO <nguyendo836660@gmail.com>
2025-05-31 20:58:39 +0700

    video 48

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import reactLogo from "./assets/react.svg";
 import { useState } from "react";
 import Header from "./component/layout/header";
 import Footer from "./component/layout/footer";
+import { Outlet } from "react-router-dom";
 const App = () => {
 
   const addNewTodo = (name) => {
@@ -61,6 +62,7 @@ const App = () => {
           </div>
         }
       </div>
+      <Outlet />
       <Footer />
     </>
   )
