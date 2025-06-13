9529da61f58ef6b742809e2c1c5b3306f07bc709
DarwinDo <nguyendo836660@gmail.com>
2025-06-13 14:24:55 +0700

    video 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,21 +1,40 @@
 import { Button, Checkbox, Form, Input } from "antd";
 
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) =>{
+        console.log("check value:", values)
+    }
     return (
         <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600 }}
-            initialValues={{ remember: true }}
-            // onFinish={onFinish}
-            // onFinishFailed={onFinishFailed}
-            autoComplete="off"
+            layout="vertical"
+            form={form}
+            // name="basic"
+            // labelCol={{ span: 8 }}
+            // wrapperCol={{ span: 16 }}
+            // style={{ maxWidth: 600 }}
+            // initialValues={{ remember: true }}
+            onFinish={onFinish}
+            // // onFinishFailed={onFinishFailed}
+            // autoComplete="off"
         >
-            <div style={{ display: 'flex', gap: '20px', flexDirection: 'column', margin: '50px' }}>
+            <div 
+            style={{ 
+            // display: 'flex', gap:'20px',  flexDirection: 'column',
+             margin: '50px'
+             }}
+            >  
                 <Form.Item
-                    label="Username"
-                    name="username"
+                    label="Full name"
+                    name="fullName"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
                 // rules={[{ required: true, message: 'Please input your username!' }]}
                 >
                     <Input />
@@ -29,13 +48,24 @@ const RegisterPage = () => {
                     <Input.Password />
                 </Form.Item>
 
+                <Form.Item
+                    label="Phone"
+                    name="phone"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
                 {/* <Form.Item name="remember" valuePropName="checked" label={null}>
                 <Checkbox>Remember me</Checkbox>
             </Form.Item> */}
 
                 <Form.Item label={null}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
+                    <Button type="primary" 
+                    // htmlType="submit" //typeSubmit: C1
+                    onClick={()=>{form.submit()}} //typeSubmit: C2
+                    >
+                        Register
                     </Button>
                 </Form.Item>
             </div>
