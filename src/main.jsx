4b6a1db9008607657d7423d3955d448c39c65bc4
DarwinDo <nguyendo836660@gmail.com>
2025-06-14 17:06:43 +0700

    video 101

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "./styles/global.css";
 import TodoApp from './component/todo/TodoApp.jsx';
 import ErrorPage from './pages/error.jsx';
 import { AuthWrapper } from './component/auth.context.jsx';
+import PrivateRoute from './pages/private.route.jsx';
 
 const router = createBrowserRouter([
   {
@@ -31,7 +32,10 @@ const router = createBrowserRouter([
       },
       {
         path: "/books",
-        element: <BookPage />,
+        element:
+          <PrivateRoute>
+            <BookPage />
+          </PrivateRoute>,
       }
     ]
   },
